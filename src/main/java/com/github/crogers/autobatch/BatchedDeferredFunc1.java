a8d1edc3b6e49b6ca18a36ee218f903c8601465b
Callum Rogers <callumrogers@hotmail.co.uk>
2017-07-09 19:50:53 +0800

    Make package private constructor

--- src/main/java/com/github/crogers/autobatch/BatchedDeferredFunc1.java
+++ src/main/java/com/github/crogers/autobatch/BatchedDeferredFunc1.java
@@ -10,7 +10,7 @@ public class BatchedDeferredFunc1<A, R> implements DeferredFunc1<A, R> {
     private final List<A> invocations = new ArrayList<>();
     private final Supplier<List<R>> savedBatch;
 
-    public BatchedDeferredFunc1(Batcher<A, R> batcher) {
+    /* package */ BatchedDeferredFunc1(Batcher<A, R> batcher) {
         this.savedBatch = Suppliers.memoize(() ->
                 batcher.batch(invocations)
         );
